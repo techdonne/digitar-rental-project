e0c92b668df922c73fb0b9b11b05ebea6382b1a8
Daniel Araújo <dannielaraujooficial@gmail.com>
2023-11-15 11:45:34 -0300

    Update AcessorioEntity.java

--- DigitalRentalProject/src/main/java/techdonne/ifma/dcomp/entities/AcessorioEntity.java
+++ DigitalRentalProject/src/main/java/techdonne/ifma/dcomp/entities/AcessorioEntity.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 
 @Entity
 @Table(name= "acessorio")
