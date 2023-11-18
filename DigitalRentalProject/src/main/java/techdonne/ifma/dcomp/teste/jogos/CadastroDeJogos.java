c78c5c181050a01cb94786b8fa1c5d4e66a8c2ba
Daniel Araújo <dannielaraujooficial@gmail.com>
2023-11-18 10:16:58 -0300

    mundando cadastro de jogos

--- DigitalRentalProject/src/main/java/techdonne/ifma/dcomp/teste/jogos/CadastroDeJogos.java
+++ DigitalRentalProject/src/main/java/techdonne/ifma/dcomp/teste/jogos/CadastroDeJogos.java
@@ -11,17 +11,39 @@ import techdonne.ifma.dcomp.repositories.JogoRepository;
 import techdonne.ifma.dcomp.repositories.PlataformaRepository;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 
 public class CadastroDeJogos {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("digital_rental_project");
         EntityManager manager = factory.createEntityManager();
+        //criando o jogo
+        JogoEntity jogo = new JogoEntity(null , "GOD OF WAR", new HashSet<>());
 
-        JogoEntity jogo = JogoEntity.builder().titulo("GOD OF WAR").build();
+        manager.getTransaction().begin(); // comeco de transação
 
-        manager.getTransaction().begin();
-
+        //objetos de repositótio de plataforma e jogo
+        PlataformaRepository plataformaRepository = new PlataformaRepository(manager);
         JogoRepository jogoRepository = new JogoRepository(manager);
+
+        //buscando plataforma de id = 1 e id = 2
+        PlataformaEntity plataforma1 = plataformaRepository.buscarPorId(PlataformaEntity.class, 1);
+        PlataformaEntity plataforma2 = plataformaRepository.buscarPorId(PlataformaEntity.class, 2);
+
+        JogoPlataformaEntity jogoPlataformaEntity1 = new JogoPlataformaEntity();
+
+        jogoPlataformaEntity1.setPrecoDiario(new BigDecimal(10));
+        jogoPlataformaEntity1.setJogo(jogo);
+        jogoPlataformaEntity1.setPlataforma(plataforma1);
+
+        JogoPlataformaEntity jogoPlataformaEntity2 = new JogoPlataformaEntity();
+
+        jogoPlataformaEntity2.setPrecoDiario(new BigDecimal(12));
+        jogoPlataformaEntity2.setJogo(jogo);
+        jogoPlataformaEntity2.setPlataforma(plataforma2);
+
+        jogo.adiciona(jogoPlataformaEntity1);
+        jogo.adiciona(jogoPlataformaEntity2);
 
         jogoRepository.salvar(jogo);
 
